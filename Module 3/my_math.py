961268b22a4f3ef1e534696dd33e271a6804fd94
whoisflynn <51035842+whoisflynn@users.noreply.github.com>
2019-08-03 22:24:34 -0600

    Create my_math.py
    This custom module was created during:
    Module 4
    Lesson 4.2

---
+++ Module 3/my_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def myAdd(a,b):
+  return a + b
+
+def mySub(a,b):
+  return a-b
+
+def myDiv(a,b):
+  return a/b
+
+def myMul(a,b):
+  return a * b
+
+def main():
+  print(myAdd(1,2))
+
+if __name__ == “__main__”:
+  main()
