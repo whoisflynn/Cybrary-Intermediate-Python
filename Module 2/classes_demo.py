b50e946d1371ab79c31554d718ca94e090db84e8
whoisflynn <51035842+whoisflynn@users.noreply.github.com>
2019-08-03 22:18:10 -0600

    Create classes_demo.py
    This script was created during:
    Module 3
    Lessons 3.1 - 3.2

---
+++ Module 2/classes_demo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/Python3
+class Person:
+  name = ‘’
+  age = 0
+  def __init__(self,name,age):
+    self.name = name
+    self.age = age
+    return
+    
+  def print_vals()
+    print(‘Name: {}\nAge: {}’.format(self.name,self.age))
+
+class Employee(Person):
+  def __init__(self, name, age, department):
+    Person.__init__(self, name, age)
+    Self.department = department
+    
+def extended_print_vals(self)
+  print(‘Name: {}\nAge: {}’.format(self.name,self.age, self.department))
+  
+  
+emp = Employee(‘Joe’, 25, ‘Research’)
+emp.extended_print_vals()
