24bb246b1e47385f96df28503b5ea3c66b7d898f
whoisflynn <whoisflynn@protonmail.com>
2019-08-05 11:38:57 -0600

    Added __init__.py

--- Module 2/except_demo.py
+++ Module 2/except_demo.py
@@ -2,21 +2,21 @@
 
 class CustomException(Exception):
   print(‘Exception found’)
-  
+
 def main():
   for i in range(10):
     if i % 3 == 0:
       raise CustomException:
-      
-‘’’
+
+
 try:
   list_one = [1,2,3,4,5]
   for i in range(6):
     print(list_one[i])
-    
+
 except IndexError:
   print(‘Index out of range’)
 
 print(‘Got past our error’)
-‘’’
+
 main()
